1abdbeaae054eb73376b9bd601e747328db4caa6
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-11-26 17:45:44 -0300

    Add files via upload

--- adivinha.sh
+++ adivinha.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+$(( RANDOM % 50 + 1 ))
+while 
+do
+	echo "Me dê um palpite de qual o número."
+	read RESPOSTA 
+	if test $RESPOSTA -lt 
+	then echo "Muito baixo"
+	else test $RESPOSTA -gt 
+	then echo "Muito alto"
+	 VEZ ++1 
+done
+	
